f8f2ca03b1006f272b677ad99aefbd0cf4058ebf
uribracha2611 <uribracha2611@gmail.com>
2023-03-25 11:21:14 +0300

    removed useeffect from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import { useEffect } from "react";
 import AuthContext from "./AuthContext";
 import BackButton from "./backButton";
 import NavBar from './navbar'
-import AppStore, { useRemindersContext } from "./store";
-import { fetchReminders } from "./utilsClient";
+import AppStore  from "./store";
 import './globals.css'
 
 export interface AccountLayoutProps {
@@ -17,14 +16,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const session = useSession();
-  const { Reminders, SetReminders } = useRemindersContext();
 
-  useEffect(() => {
-    if (session && session != undefined && session.status == "authenticated" && Reminders.length == 0) {
-      fetchReminders();
-    };
-  }, [session]);
 
   return (
     <html lang="en">
